02cb3b6838ddf4c2e20e0776eeafb06a426b1af0
ismael <ismaelcontreras4377@gmail.com>
2023-10-04 17:48:49 -0600

    Add Iterative DFS, Create Board Class

---
+++ src/Board.java
@@ -0,0 +1,98 @@
+import java.util.*;
+
+public class Board {
+
+    private final HashMap<List<Integer>, Integer> visitedBoards = new HashMap<>();
+    private final String UP = "up";
+    private final String DOWN = "down";
+    private final String LEFT = "left";
+    private final String RIGHT = "right";
+    private List<Integer> goal;
+    private List<Integer> currentBoard;
+    int n = 3;
+    Integer nodesExamined = 0;
+    Stack<String> solutionMoves = new Stack<>();
+    Integer depthCount = 26;
+    int count = 0;
+
+    public boolean boardsMatch(){
+        return currentBoard.equals(goal);
+    }
+    public void setBoards(List<Integer> goalBoard, List<Integer> startBoard){
+        goal = goalBoard;
+        currentBoard = startBoard;
+    }
+
+    public void printBoards(){
+        printBoard(currentBoard, "Start");
+        printBoard(goal, "Goal");
+    }
+
+    private void printBoard(List<Integer> board, String name){
+        System.out.println("\n" + name + ":");
+        for (int i = 0; i < board.size(); i++){
+            if( i % n == 0 && i != 0){
+                System.out.println();
+            }
+            System.out.print(board.get(i) + " ");
+        }
+        System.out.println();
+    }
+
+    public void makeMove(String move){
+        int indexOfZero = currentBoard.indexOf(0);
+
+        if (Objects.equals(move, UP)){
+            currentBoard.set(indexOfZero, currentBoard.get(indexOfZero - n));
+            currentBoard.set(indexOfZero-n, 0);
+        } else if (Objects.equals(move, DOWN)) {
+            currentBoard.set(indexOfZero, currentBoard.get(indexOfZero + n));
+            currentBoard.set(indexOfZero+n, 0);
+        } else if (Objects.equals(move, RIGHT)) {
+            currentBoard.set(indexOfZero, currentBoard.get(indexOfZero+1));
+            currentBoard.set(indexOfZero+1, 0);
+        } else if (Objects.equals(move, LEFT)){
+            currentBoard.set(indexOfZero, currentBoard.get(indexOfZero-1));
+            currentBoard.set(indexOfZero-1, 0);
+        }
+    }
+
+    public void undoMove(String move){
+        if (Objects.equals(move, UP)){
+            makeMove(DOWN);
+        } else if (Objects.equals(move, DOWN)) {
+            makeMove(UP);
+        } else if (Objects.equals(move, RIGHT)) {
+            makeMove(LEFT);
+        } else if (Objects.equals(move, LEFT)){
+            makeMove(RIGHT);
+        }
+    }
+
+    public List<String> getMovesList(String prevMove){
+        List<String> validMoves = new ArrayList<>();
+        int indexOfZero = currentBoard.indexOf(0);
+
+        if (!Objects.equals(prevMove, RIGHT) && indexOfZero % n > 0 ){
+            validMoves.add(LEFT);
+        }
+        if (!Objects.equals(prevMove, LEFT) && indexOfZero % n < n-1){
+            validMoves.add(RIGHT);
+        }
+        if (!Objects.equals(prevMove, DOWN) && indexOfZero >= n){
+            validMoves.add(UP);
+        }
+        if (!Objects.equals(prevMove, UP) && indexOfZero < currentBoard.size() - n){
+            validMoves.add(DOWN);
+        }
+        return validMoves;
+    }
+
+    public boolean visited(){
+        return (visitedBoards.containsKey(currentBoard) && visitedBoards.get(currentBoard) < count);
+    }
+
+    public void addToVisitedBoards(){
+        visitedBoards.put(currentBoard, count);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,134 +2,70 @@ import java.util.*;
 
 public class  Main {
 
-    static String UP = "up";
-    static String DOWN = "down";
-    static String LEFT = "left";
-    static String RIGHT = "right";
-    static int n = 3;
-    static Integer nodesExamined = 0;
-    static HashMap<List<Integer>, Integer> visitedBoards = new HashMap<>();
-    static Stack<String> solutionMoves = new Stack<String>();
-    static Integer depthCount = 26;
     public static void main(String[] args) {
 
         List<Integer> goal = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8);
-        List<Integer> recursStart = Arrays.asList(7, 2, 4, 5, 0, 6, 8, 3, 1);;
-        List<Integer> itStart = Arrays.asList(7, 2, 4, 5, 0, 6, 8, 3, 1);;
-        int count = 0;
-        printBoard(recursStart, "Start");
-        printBoard(goal, "Goal");
-        Boolean solutionFound = backtrackingDFS(recursStart, goal, "", count);
-        System.out.println("Nodes Examined = " + nodesExamined);
-        while (!solutionMoves.empty()){
-            System.out.println(solutionMoves.pop());
+        List<Integer> start = Arrays.asList(7, 2, 4, 5, 0, 6, 8, 3, 1);
+        List<Integer> start1 = Arrays.asList(7, 2, 4, 5, 0, 6, 8, 3, 1);
+        Board dfsBoard = new Board();
+        dfsBoard.setBoards(goal, start);
+        dfsBoard.printBoards();
+        dfsBoard.n = 3;
+
+        backtrackingDFS(dfsBoard, "");
+        System.out.println("Nodes Examined = " + dfsBoard.nodesExamined);
+        while (!dfsBoard.solutionMoves.empty()){
+            System.out.println(dfsBoard.solutionMoves.pop());
         }
-        iterativeDFS(itStart, goal);
+
+        Board itBoard = new Board();
+        itBoard.setBoards(goal, start1);
+        itBoard.printBoards();
+        itBoard.n = 3;
+        iterativeDFS(itBoard);
     }
 
-    private static void printBoard(List<Integer> board, String boardName){
-        System.out.println("\n" + boardName + ":");
-        for (int i = 0; i < board.size(); i++){
-            if( i % n == 0 && i != 0){
-                System.out.println();
-            }
-            System.out.print(board.get(i) + " ");
-        }
-        System.out.println();
+    private static Boolean backtrackingDFS(Board dfsBoard, String prevMove){
 
-    }
-
-    private static List<String> getMovesList(List<Integer> board, String prevMove){
-        List<String> validMoves = new ArrayList<String>();
-        int indexOfZero = board.indexOf(0);
-
-        if (!Objects.equals(prevMove, RIGHT) && indexOfZero % n > 0 ){
-            validMoves.add(LEFT);
-        }
-        if (!Objects.equals(prevMove, LEFT) && indexOfZero % n < n-1){
-            validMoves.add(RIGHT);
-        }
-        if (!Objects.equals(prevMove, DOWN) && indexOfZero >= n){
-            validMoves.add(UP);
-        }
-        if (!Objects.equals(prevMove, UP) && indexOfZero < board.size() - n){
-            validMoves.add(DOWN);
-        }
-
-        return validMoves;
-    }
-
-    private static void makeMove(List<Integer> board, String move){
-        int indexOfZero = board.indexOf(0);
-
-        if (Objects.equals(move, UP)){
-            board.set(indexOfZero, board.get(indexOfZero - n));
-            board.set(indexOfZero-n, 0);
-        } else if (Objects.equals(move, DOWN)) {
-            board.set(indexOfZero, board.get(indexOfZero + n));
-            board.set(indexOfZero+n, 0);
-        } else if (Objects.equals(move, RIGHT)) {
-            board.set(indexOfZero, board.get(indexOfZero+1));
-            board.set(indexOfZero+1, 0);
-        } else if (Objects.equals(move, LEFT)){
-            board.set(indexOfZero, board.get(indexOfZero-1));
-            board.set(indexOfZero-1, 0);
-        }
-    }
-
-    private static void undoMove(List<Integer> board, String move){
-        if (Objects.equals(move, UP)){
-            makeMove(board, DOWN);
-        } else if (Objects.equals(move, DOWN)) {
-            makeMove(board, UP);
-        } else if (Objects.equals(move, RIGHT)) {
-            makeMove(board, LEFT);
-        } else if (Objects.equals(move, LEFT)){
-            makeMove(board, RIGHT);
-        }
-    }
-
-    private static Boolean backtrackingDFS(List<Integer> board, List<Integer> goal, String prevMove, int count){
-
-        if (visitedBoards.containsKey(board) && visitedBoards.get(board) < count){
+        if (dfsBoard.visited()){
             return false;
         }
         else {
-            visitedBoards.put(board, count);
+            dfsBoard.addToVisitedBoards();
         }
-        nodesExamined += 1;
-        if (count > depthCount){
+        dfsBoard.nodesExamined += 1;
+        if (dfsBoard.count > dfsBoard.depthCount){
             return false;
         }
-        if (board.equals(goal)){
-            System.out.println("Solution Length = " + count);
+        if (dfsBoard.boardsMatch()){
+            System.out.println("Solution Length = " + dfsBoard.count);
             return true;
         }
-        List<String> movesList = getMovesList(board, prevMove);
+        List<String> movesList = dfsBoard.getMovesList(prevMove);
         for (String move : movesList){
-            makeMove(board, move);
-            count +=1;
-            if (backtrackingDFS(board, goal, move, count)) {
-                solutionMoves.push(move);
+            dfsBoard.makeMove(move);
+            dfsBoard.count +=1;
+            if (backtrackingDFS(dfsBoard, move)) {
+                dfsBoard.solutionMoves.push(move);
                 return true;
             }
-            undoMove(board, move);
-            count -=1;
+            dfsBoard.undoMove(move);
+            dfsBoard.count -=1;
         }
         return false;
     }
 
-    private static void iterativeDFS(List<Integer> board, List<Integer> goal){
-        depthCount = 0;
-        nodesExamined = 1;
+    private static void iterativeDFS(Board itBoard){
+        itBoard.depthCount = 0;
+        itBoard.nodesExamined = 1;
         boolean solutionFound = false;
         while(!solutionFound){
-            System.out.println(depthCount + ": Cumulative nodes-examined = " + nodesExamined);
-            solutionFound = backtrackingDFS(board, goal, "", 0);
-            depthCount += 1;
+            System.out.println(itBoard.depthCount + ": Cumulative nodes-examined = " + itBoard.nodesExamined);
+            solutionFound = backtrackingDFS(itBoard, "");
+            itBoard.depthCount += 1;
         }
-        while (!solutionMoves.empty()){
-            System.out.println(solutionMoves.pop());
+        while (!itBoard.solutionMoves.empty()){
+            System.out.println(itBoard.solutionMoves.pop());
         }
     }
 }
